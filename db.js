c4e73d11c2f831feec0a07d4b33ce89957946cdb
herokuteamacc <64529699+herokuteamacc@users.noreply.github.com>
2020-06-15 01:07:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ var sequelize = new Sequelize(
     config.dbConnectionString.username,
     config.dbConnectionString.password,
     config.dbConnectionString);*/
-var sequelize = new Sequelize(process.env[config.database_url]);
+var sequelize = new Sequelize(process.env[config.dbConnectionString.database_url]);
 require('sequelize-values')(sequelize);
 
 module.exports = sequelize;
