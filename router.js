ebd0663e1843d4a269fc39571ae7a9fb8b663475
herokuteamacc <64529699+herokuteamacc@users.noreply.github.com>
2020-06-20 16:47:19 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,10 +1,14 @@
+console.log('Inside router');
 const authController = require('./controllers/auth');
-
+console.log('Declared authcontroller - router.js');
 const db  = require('./models/index'); //for db connection  
+console.log('required db - router.js');
+
 module.exports.set = (app) => {
+	console.log('Will I be just called?');
 	app.get('/api/user', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        //console.log(db);
+        console.log('inside api user method');
         let data = db.User.findOne({}).then(d => {
             res.send(JSON.stringify(d));
         })
@@ -13,7 +17,7 @@ module.exports.set = (app) => {
             res.send(JSON.stringify({'error':err}));
         })
     });
-
+	console.log('After api user , Before login and register');
     app.post('/api/login', authController.login);
     app.post('/api/register', authController.register);
     //app.get('/api/posts', postController.post);
