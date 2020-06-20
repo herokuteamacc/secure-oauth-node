27203590018a2e4bbdf62e54ed6c6fb4d3470913
herokuteamacc <64529699+herokuteamacc@users.noreply.github.com>
2020-06-20 16:40:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log('Start of the point');
+
 const path = require('path');
 const express = require('express'); 
 const app = express();
@@ -7,12 +8,21 @@ const router = require('./router');
 const bodyParser = require('body-parser');
 const config = require('./config');
 
+console.log('Done all requirements');
+
 app.use(bodyParser.json());
+
+console.log('Used BodyParser');
+
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+console.log('Used BodyParser - extended true');
+
 router.set(app);
+
+console.log('Set the app done');
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -20,7 +30,10 @@ app.use(function(req, res, next) {
 	next();
   });
 
+console.log('Used app.use with all next() functions');
+
 app.use('/', express.static(__dirname + '/'));
+
 console.log('Start of the point - End calling index.html');
 
 app.get('*', (req, res) => {
