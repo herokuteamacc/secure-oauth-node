be11eebc704a83daf15ded49b43f7164be6e6569
Adkrishna <encaditya@gmail.com>
2020-08-18 04:18:15 +0530

    Adding Heroku config var

--- controllers/verifyToken.js
+++ controllers/verifyToken.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 const fs   = require('fs');
 const path   = require('path');
-const publicKEY = require('fs').readFileSync(path.resolve(__dirname, '../', 'config', 'certs', 'publickey.pem'), 'utf8');
+const publicKEY = fs.readFileSync(path.resolve(__dirname, '../', 'config', 'certs', 'publickey.pem'), 'utf8');
 
 function tokenVerifyUsingPublicKey(token){
 
 var verifyOptions = {
-    expiresIn:  '12h',
-    algorithm:  ["RS256"]
+    expiresIn:  process.env.EXPIRES_IN,
+    algorithm:  process.env.ALGORITHM
    };
     const decodedToken = jwt.verify(token, publicKEY,verifyOptions);
     console.log("\nJWT verification result: " + JSON.stringify(decodedToken));
