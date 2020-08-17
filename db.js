1c07259b5dcfe7f5c17b081ad4034c85c1fd0db5
Adkrishna <encaditya@gmail.com>
2020-08-18 03:07:09 +0530

    Adding Heroku config var

--- db.js
+++ db.js
@@ -6,8 +6,7 @@ var sequelize = new Sequelize(
     config.dbConnectionString.username,
     config.dbConnectionString.password,
     config.dbConnectionString);*/
-//var sequelize = new Sequelize(process.env[config.dbConnectionString.database_url]); Aditya - commenting 18th Aug 
-var sequelize = new Sequelize(process.env.DATABASE_URL);
+    var sequelize = new Sequelize(config.dbConnectionString);
 require('sequelize-values')(sequelize);
 
 module.exports = sequelize;
